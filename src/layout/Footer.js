cd45833c1f6a45e0e0a870418480b4659a0b5df8
Divya Maheshwari <60094383+divya814@users.noreply.github.com>
2021-01-25 00:10:16 +0530

    Update Footer.js

--- src/layout/Footer.js
+++ src/layout/Footer.js
@@ -3,7 +3,7 @@ import {Container} from "reactstrap"
 
 export default function Footer() {
     return (
-        <Container style={{background: "linear-gradient(to right, #538FFB, #5737D6)" }} fluid tag="footer" className="text-center mt-3 bg-info text-white text-uppercase fixed-bottom p-3">
+        <Container style={{background: "linear-gradient(to right, #538FFB, #5737D6)" }} fluid tag="footer" className="text-center mt-4 bg-info text-white text-uppercase position-absolute bottom-0 p-3">
             Developed by Divya Maheshwari
         </Container>
     )
